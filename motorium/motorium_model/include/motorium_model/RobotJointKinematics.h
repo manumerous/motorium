5363bf6bfebc96c2b529fd094055954817cba153
Manuel Yves Galliker <manuel.galliker@gmx.ch>
2026-02-01 21:54:01 +0100

    Add JointKinematics

---
+++ motorium/motorium_model/include/motorium_model/RobotJointKinematics.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+Copyright (c) 2025, Manuel Yves Galliker. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#pragma once
+#include <motorium_core/Types.h>
+#include <motorium_model/JointIDMap.h>
+#include <motorium_model/RobotDescription.h>
+
+namespace motorium::model {
+
+struct JointAction {
+  scalar_t q_des = 0;
+  scalar_t qd_des = 0;
+  scalar_t kp = 0;
+  scalar_t kd = 0;
+  scalar_t feed_forward_effort = 0;
+
+  scalar_t getTotalFeedbackTorque(scalar_t q, scalar_t qd) const { return (kp * (q_des - q) + kd * (qd_des - qd) + feed_forward_effort); }
+};
+
+class RobotJointAction : public JointIdMap<JointAction> {
+ public:
+  explicit RobotJointAction(const RobotDescription& robot_description) : JointIdMap(robot_description) {}
+};
+
+}  // namespace motorium::model
