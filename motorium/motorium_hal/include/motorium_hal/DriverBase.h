cb1a2388c2f6c38b1ec3266460997fb1fbd436b8
Manuel Yves Galliker <manuel.galliker@gmx.ch>
2026-04-24 18:41:11 +0200

    Small dependency updates

--- motorium/motorium_hal/include/motorium_hal/DriverBase.h
+++ motorium/motorium_hal/include/motorium_hal/DriverBase.h
@@ -30,6 +30,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #pragma once
 
 #include <string>
+#include <utility>
 #include <vector>
 
 #include <motorium_core/StateMachine.h>
